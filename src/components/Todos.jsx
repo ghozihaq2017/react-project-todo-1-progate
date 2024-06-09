997f28f52b659761458d3fbb416c01140f474816
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-06-09 21:53:52 +0700

    Add CSS to components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react'
-import TodoItem from './TodoItem' 
+import React from 'react';
+import TodoItem from './TodoItem';
 
 const Todos = ({ todos }) => {
   return (
-    <div>
+    <div style={styles.container}>
       {todos.map((todo) => {
-        // Berikan datanya ke TodoItem
-        return <TodoItem key={todo.id} todo={todo} />
+        return <TodoItem key={todo.id} todo={todo} />;
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+const styles = {
+  container: {
+    width: '40%',
+    margin: '0 auto'
+  }
+};
+
+export default Todos;
